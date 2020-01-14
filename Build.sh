7441529e72199b1bdddf86ec69ec620261a16e5d
andrews-viji <46959754+andrews-viji@users.noreply.github.com>
2020-01-14 14:11:18 +0530

    build step

---
+++ Build.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+# run container
+run () {
+    docker run --name python -d -p 80:80 python_test
+}
+# stop and remove container
+stop_rm () {
+    docker stop python
+    docker rm python
+}
+# build new image for the latest build
+docker build -t python_test .
+# check if the production contaier is not running
+if [ ! "$(docker ps -q -f name=python)" ];then
+    # cehck if the container is in docker ps -a
+    if [ "$(docker ps -aq -f status=exited -f name=python)" ];then
+        docker rm python
+    fi
+    run
+else
+    stop_rm
+    run
+fi
+# remove <none>:<none> image in docker images
+if [ "$(docker images -f "dangling=true" -q" ];then
+    docker rmi $(docker images -f "dangling=true" -q)
+fi
